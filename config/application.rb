a5da8ce529ab4682459b7a5817bd5638bdb84cdf
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-10-25 00:12:57 +0900

    ジェネレーター修正

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
-      g.skip_routes do |g|
+      g.skip_routes true
       g.helper false
       g.test_framework nil
     end    
