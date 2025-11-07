523462aa13ec4a5c6c26f4246fa2181f6d0adb68
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-11-07 12:13:46 +0900

    フラッシュメッセージに色を付けました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :danger
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :convert_devise_flash_messages
+
+  private
+
+  def convert_devise_flash_messages
+    if flash[:notice]
+      flash[:success] = flash[:notice]
+      flash.delete(:notice)
+    end
+
+    if flash[:alert]
+      flash[:danger] = flash[:alert]
+      flash.delete(:alert)
+    end
+  end    
 end
