1a789024dd36ab32ab95c8cbf71db0e79ede30e6
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-11-12 11:17:33 +0900

    作品新規作成機能の実装

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,24 @@ class WorksController < ApplicationController
   def index
     @works = Work.includes(:user)
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = current_user.works.build(work_params)
+    if @work.save
+      redirect_to works_path, success: "作品の新規作成に成功しました"
+    else
+      flash.now[:danger] = "作品の新規作成に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_11_004410) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
