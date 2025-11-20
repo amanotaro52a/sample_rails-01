bb112933f126b1334700ed9a9ad38624562b6622
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-11-21 00:48:23 +0900

    タイトルの動的出力

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_19_044538) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -40,7 +39,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_19_044538) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "image_data"
+    t.string "image"
     t.index ["user_id"], name: "index_works_on_user_id"
   end
 
