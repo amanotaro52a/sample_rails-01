eed023d404540728b3f911a41aac96eba2e8aaad
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-12-01 18:01:20 +0900

    routes.rbのタイポ修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       get :bookmarks
     end
   end
-  mount LetterOpenerWeb::Engine, at:"/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :bookmarks, only: %i[create destroy]
   resource :profile, only: %i[show edit update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
