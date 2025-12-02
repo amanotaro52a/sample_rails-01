213ad0aaa37b324ee4cfb2ee8f9d6f70650a4c02
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-12-02 15:52:15 +0900

    パスワードリセット機能 本機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_28_010622) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -50,7 +52,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_28_010622) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
+    t.text "image_data"
     t.index ["user_id"], name: "index_works_on_user_id"
   end
 
