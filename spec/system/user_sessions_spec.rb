255caf739348681bcef7297a52a39380dbe881d3
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2026-01-22 11:17:22 +0900

    user_sessionテストコード

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常な場合' do
+      it 'ログイン処理が成功する', js: true do
+        visit new_user_session_path
+
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'ログイン'
+
+        expect(page).to have_current_path(root_path)
+
+        find('#header-profile').click
+        expect(page).to have_link('ログアウト')
+      end
+    end
+
+    context 'フォームの値が未入力の場合' do
+      it 'ログインに失敗する', js: true do
+        visit new_user_session_path
+
+        fill_in 'user[email]', with: ''
+        fill_in 'user[password]', with: ''
+        click_button 'ログイン'
+
+        expect(page).to have_current_path(new_user_session_path)
+        expect(page).to have_button('ログイン')
+        expect(page).not_to have_link('ログアウト')
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    before do
+      visit new_user_session_path
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+    end
+
+    it 'ログアウトされる', js: true do
+      find('#header-profile').click
+      click_link 'ログアウト'
+
+      expect(page).to have_current_path(new_user_session_path)
+      expect(page).to have_link('ログイン')
+      expect(page).not_to have_link('ログアウト')
+    end
+  end
 end
